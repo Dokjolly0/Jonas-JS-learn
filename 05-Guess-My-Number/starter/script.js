03e4757a94b06bdbeef65b76df25ed9855fb2c36
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-02-01 10:17:02 +0100

    Prova commit automatici

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,5 @@
 
 //querySelector ti prende tutto l'elemento, se ci aggiungi .textContent ti salvera solo il valore
 console.log(document.querySelector('.message').textContent);
+
+console.log('Ciao');
