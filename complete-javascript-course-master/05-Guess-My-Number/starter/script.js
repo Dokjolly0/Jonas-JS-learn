3a5d30cd01b26b2a1fe3af315005ba0f42465513
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-02-03 15:08:03 +0100

    Issue resolved

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,32 +1,33 @@
 'use strict';
 const check = document.querySelector('.check');
 let verificaNumero;
+let punteggio = 20;
 const sicretNumber = Math.trunc(Math.random() * 20) + 1;
 
+//document.querySelector('.number').textContent = sicretNumber;
 //Perche si registri il valore nella variabile bisogna metterlo dentro la stessa funzione dove poi si usa quella variabile
 check.addEventListener('click', function () {
   const numeroSelezionato = Number(document.querySelector('.guess').value);
-  let punteggio = Number(document.querySelector('.score').value);
-  punteggio = 20;
   console.log(numeroSelezionato);
-  //#region if !variabile
-  //Se è diverso da true (lo 0 è sempre false) allora fai...
-  //   if (!numeroSelezionato) {
-  //     document.querySelector('.message').textContent = '⛔Numero non valido!';
-  //     verificaNumero = 0;
-  //   }
-  //#endregion
+
   if (sicretNumber === numeroSelezionato) {
-    document.querySelector('.message').textContent = '🥳Numero indovinato!!!';
+    document.querySelector('.message').textContent = '🥳 Numero indovinato!!!';
   } else if (sicretNumber > numeroSelezionato && numeroSelezionato !== 0) {
-    document.querySelector('.message').textContent = '🕵️‍♀️Numero troppo basso!';
-    punteggio = punteggio - 1;
+    document.querySelector('.message').textContent = '🕵️‍♀️ Numero troppo basso!';
+    punteggio--;
     document.querySelector('.score').textContent = punteggio;
   } else if (sicretNumber < numeroSelezionato && numeroSelezionato !== 0) {
-    document.querySelector('.message').textContent = '🕵️‍♀️Numero troppo alto!';
-    punteggio = punteggio - 1;
+    document.querySelector('.message').textContent = '🕵️‍♀️ Numero troppo alto!';
+    punteggio--;
     document.querySelector('.score').textContent = punteggio;
-  } else if (numeroSelezionato === 0) {
-    document.querySelector('.message').textContent = '⛔Numero non valido!';
+  } else if (!numeroSelezionato) {
+    //Se è diverso da true (lo 0 è sempre false) allora fai...
+    document.querySelector('.message').textContent = '⛔ Numero non valido!';
+  }
+
+  if (punteggio < 0) {
+    document.querySelector('.message').textContent = '💀 Hai perso!';
+    punteggio++;
+    document.querySelector('.score').textContent = punteggio;
   }
 });
